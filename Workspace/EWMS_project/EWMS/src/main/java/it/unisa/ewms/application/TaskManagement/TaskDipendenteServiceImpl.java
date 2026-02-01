4091c4f8fa75250670fe578e9979c9b712675d43
V1NC104 <v.mauro12@studenti.unisa.it>
2026-02-01 18:30:07 +0100

    Add: aggiornati i metodi del task management service come indicato nella nuova versione del SDD

--- Workspace/EWMS_project/EWMS/src/main/java/it/unisa/ewms/application/TaskManagement/TaskDipendenteServiceImpl.java
+++ Workspace/EWMS_project/EWMS/src/main/java/it/unisa/ewms/application/TaskManagement/TaskDipendenteServiceImpl.java
@@ -3,11 +3,15 @@ package it.unisa.ewms.application.TaskManagement;
 import it.unisa.ewms.application.TaskManagement.interfaces.TaskDipendenteService;
 import it.unisa.ewms.model.beans.Task;
 import it.unisa.ewms.model.beans.Tipi;
+import it.unisa.ewms.model.beans.Utente;
 import it.unisa.ewms.persistance.PersistenceServiceImpl;
 import it.unisa.ewms.persistance.interfaces.ITaskDAO;
+import it.unisa.ewms.persistance.interfaces.IUtenteDAO;
 import it.unisa.ewms.persistance.interfaces.PersistenceService;
 
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
 
 public class TaskDipendenteServiceImpl implements TaskDipendenteService {
     @Override
@@ -38,5 +42,17 @@ public class TaskDipendenteServiceImpl implements TaskDipendenteService {
                 return true;
             }catch(Exception e){throw new SQLException(e);}
         }
+    }
+
+    @Override
+    public List<Task> getAllTaskDip(String dipendenteId) throws SQLException {
+        PersistenceService service = PersistenceServiceImpl.getInstance();
+        ITaskDAO taskDAO = service.getTaskDAO();
+        IUtenteDAO utenteDAO = service.getUtenteDAO();
+
+        try{
+            Utente user = utenteDAO.findByMatricola(dipendenteId);
+            return taskDAO.findByUtente(user);
+        }catch(Exception e){throw new SQLException(e);}
     }
 }
--- Workspace/EWMS_project/EWMS/src/main/java/it/unisa/ewms/application/TaskManagement/interfaces/TaskDipendenteService.java
+++ Workspace/EWMS_project/EWMS/src/main/java/it/unisa/ewms/application/TaskManagement/interfaces/TaskDipendenteService.java
@@ -1,8 +1,12 @@
 package it.unisa.ewms.application.TaskManagement.interfaces;
 
+import it.unisa.ewms.model.beans.Task;
+
 import java.sql.SQLException;
+import java.util.List;
 
 public interface TaskDipendenteService {
     boolean inizializzaTask(long taskId) throws SQLException;
     boolean completeTask(long taskId) throws SQLException;
+    List<Task> getAllTaskDip(String dipendenteId) throws SQLException;
 }
